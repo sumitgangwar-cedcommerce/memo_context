cea3b7c0af790486b3bdac1c24824c8b902376c4
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-07-22 11:38:57 +0530

    Update App.js

--- number_to_words/src/App.js
+++ number_to_words/src/App.js
@@ -8,13 +8,59 @@ function App() {
 
   const [word , setword] = useState('')
 
+   const ones = [' zero ',' one ', ' two ', ' three ' , ' four ' , ' five ' , ' six ' , ' seven ' , ' eight ' , ' nine ',' ten ',' eleven ' , ' twelve ' , ' thirteen ' , ' fourteen ' , ' fifteen ' , ' sixteen ' ,' seventeen ' ,' eightteen ' , ' nineteen ' ]
+   const tens = ['twenty' , 'thirty' , 'fourty' , 'fifty' , 'sixty' , 'seventy' , 'eighty' , 'ninety']
+   const htlc = [' hundred ' , 'thousand ' , ' lakh ' , ' crore']
+
 
   const changeHandler = (e) => {
-    let num = Number(e.target.value);
-    setword(converter.toWords(num));
+    let num = e.target.value
+    let counter = 0
+    let str = ''
+
+    for(let i = num.length-1; i > -1; i--) {
+      if(counter===3)  str+=','
+      if(counter%2===1 && counter>3)  str+=','
+      counter++;
+      str+=num[i]  
+     
+    }
+    str = str.split(',')
+    //console.log(str)
+    let res = '';
+    for(let i = 0; i < str.length;i++){
+      let temp = str[i].split('')
+      temp = temp.reverse()
+      temp = temp.toString().replace(',','')
+      if(i===0){
+        temp = temp.split(',').toString().replace(',','')
+        if(Number(temp)<99) res += give99(temp)
+        else res+=ones[Number(temp[0])] + htlc[0] +give99(temp[1]+temp[2])
+      }
+      else{
+        if(Number(temp)!==0)  res = give99(temp) + htlc[i] + res
+      }
+    }
+
+    setword(res)
+    
   }
 
- 
+  const give99 = (temp1)=> {
+    let temp = temp1
+    console.log(Number(temp))
+    if(temp[0]==='0') temp = temp1.substring(1)
+    if(Number(temp)===0)  return ''
+    else{
+      if(Number(temp)<20) return ones[Number(temp)]
+      else if(temp[1]==='0')  return tens[Number(temp[0])-2]
+      else return  tens[Number(temp[0])-2] + ones[Number(temp[1])]
+    }
+    
+  }
+
+  
+
   return (
     <div className="App">
       <p>Please Enter Number</p>
